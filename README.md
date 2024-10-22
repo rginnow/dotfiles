792f5dd93ce0dea12002e3e57043209d95b479ab
Ryan Ginnow <rginnow@dsqtechnology.com>
2024-10-22 13:58:02 -0400

    Updates to apply to new macbook

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # My dotfiles
 
 My configs, scripts, and everything else I need
+
+> **Note:** This is a work in progress
+> it was originally built as a system-agnostic setup but
+> now it will only work on macOS.
--- zsh/install_plugins.sh
+++ zsh/install_plugins.sh
@@ -5,21 +5,28 @@ ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 POWERLEVEL10K="$ZSH_CUSTOM/themes/powerlevel10k"
 ZSH_AUTOSUGGEST="$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 ZSH_SYNTAX="$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+ARTISAN="$ZSH_CUSTOM/plugins/artisan"
 
 if [ ! -d "$POWERLEVEL10K" ]; then
     git clone https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
 else
-    echo "Powerlevel10k already installed!"
+    echo "Powerlevel10k Theme already installed!"
 fi
 
 if [ ! -d "$ZSH_AUTOSUGGEST" ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions.git "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
 else
-    echo "Autosuggest already installed!"
+    echo "ZSH Autosuggestions Plugin already installed!"
 fi
 
 if [ ! -d "$ZSH_SYNTAX" ]; then
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
 else
-    echo "Syntax Highlighting already installed!"
+    echo "ZSH Syntax Highlighting Plugin already installed!"
+fi
+
+if [ ! -d "$ARTISAN" ]; then
+    git clone https://github.com/jessarcher/zsh-artisan.git "$ZSH_CUSTOM/plugins/artisan"
+else
+    echo "ZSHArtisan Plugin already installed!"
 fi
